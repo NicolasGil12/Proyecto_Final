12f81177561ac4f8ea357dbac9f1cc8ec8052b79
Nicolas Gil <nicolas.gil@uptc.edu.co>
2023-05-28 23:31:07 -0500

    second-commit

--- GraphsGame/play.py
+++ GraphsGame/play.py
@@ -13,7 +13,6 @@ class Pathfinder:
 		self.grid = Grid(matrix = matrix)
 		self.model_icon = pygame.image.load('GraphsGame/img/selection.png').convert_alpha()
 
-		# busca do caminho
 		self.path = []
 
 		# Roomba
@@ -24,8 +23,8 @@ class Pathfinder:
 
 	def show_trace(self):
 		mouse_pos = pygame.mouse.get_pos()
-		row =  mouse_pos[1] // 32 # Posição exata do mouse na linha
-		col =  mouse_pos[0] // 32 # Posição exata do mouse na coluna
+		row =  mouse_pos[1] // 32 
+		col =  mouse_pos[0] // 32 
 		actual_value = self.matrix[row][col]
 		if actual_value == 1:
 			rect = pygame.Rect((col * 32,row * 32),(32,32))
@@ -42,13 +41,13 @@ class Pathfinder:
 		end_x,end_y =  mouse_pos[0] // 32, mouse_pos[1] // 32
 		end = self.grid.node(end_x,end_y)
 
-		# o caminho é recalculado a cada click
+		
 		finder = AStarFinder(diagonal_movement = DiagonalMovement.always)
 		self.path,_ = finder.find_path(inicio,end,self.grid)
 		self.grid.cleanup()
 		self.game.sprite.define_path(self.path)
 
-	def draw_trace(self): # Mostra o caminho a ser percorrido
+	def draw_trace(self): 
 		if self.path:
 			points = []
 			for point in self.path:
@@ -62,29 +61,29 @@ class Pathfinder:
 		self.show_trace()
 		self.draw_trace()
 
-		# roomba updating and drawing
+		
 		self.game.update()
 		self.game.draw(screen)
 
 class Game(pygame.sprite.Sprite):
 	def __init__(self,empty_path):
 
-		# base
+		
 		super().__init__()
 		self.image = pygame.image.load('GraphsGame/img/character.png').convert_alpha()
 		self.rect = self.image.get_rect(center = (60,60))
 
-		# movimento
+		
 		self.pos = self.rect.center
 		self.speed = 3
 		self.direction = pygame.math.Vector2(0,0)
 
-		# caminho
+		
 		self.path = []
 		self.collision_rects = []
 		self.empty_path = empty_path
 
-	def get_coordinate(self): #Transforma a posição atual em uma coordenada
+	def get_coordinate(self): 
 		col = self.rect.centerx // 32
 		row = self.rect.centery // 32
 		return (col,row)
@@ -94,7 +93,7 @@ class Game(pygame.sprite.Sprite):
 		self.create_collision_rects()
 		self.get_direction()
 
-	def create_collision_rects(self): # Identificando colisão no mapa
+	def create_collision_rects(self): 
 		if self.path:
 			self.collision_rects = []
 			for point in self.path:
@@ -116,7 +115,7 @@ class Game(pygame.sprite.Sprite):
 		if self.collision_rects:
 			for rect in self.collision_rects:
 				if rect.collidepoint(self.pos):
-					del self.collision_rects[0] # Remove o caminho quando alcança o objetivo
+					del self.collision_rects[0] 
 					self.get_direction()
 		else:
 			self.empty_path()
@@ -126,11 +125,11 @@ class Game(pygame.sprite.Sprite):
 		self.check_collisions()
 		self.rect.center = self.pos
 
-# base inicial
+
 pygame.init()
 screen = pygame.display.set_mode((1280,736))
 clock = pygame.time.Clock()
-# mapa
+
 
 bg_surf = pygame.image.load('GraphsGame/img/pixelmap.png').convert()
 
@@ -165,7 +164,7 @@ while True:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-		if event.type == pygame.MOUSEBUTTONDOWN: # Cria o caminho a cada click
+		if event.type == pygame.MOUSEBUTTONDOWN: 
 			pathfinder.create_path()
 
 	screen.blit(bg_surf,(0,0))
